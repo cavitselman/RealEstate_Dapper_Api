dfb63ec85377ccf5c2e8c75d27b1b5e1463325fb
Cavit Selman <tetikci462@hotmail.com>
2024-07-27 12:27:52 +0300

    Themes added.

---
+++ RED.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RED.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
