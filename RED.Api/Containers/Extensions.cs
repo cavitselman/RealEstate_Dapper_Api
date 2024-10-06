c2bbfc3c4b6629d5592dc7f7cdaab8e012cb8fbe
Cavit Selman <tetikci462@hotmail.com>
2024-10-07 01:31:12 +0300

    Extension class created.

---
+++ RED.Api/Containers/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using RED.Api.Models.DapperContext;
+using RED.Api.Repositories.AppUserRepositories;
+using RED.Api.Repositories.BottomGridRepositories;
+using RED.Api.Repositories.CategoryRepositories;
+using RED.Api.Repositories.ContactRepositories;
+using RED.Api.Repositories.EmployeeRepositories;
+using RED.Api.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories;
+using RED.Api.Repositories.EstateAgentRepositories.DashboardRepositories.LastProductsRepositories;
+using RED.Api.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticRepositories;
+using RED.Api.Repositories.MessageRepositories;
+using RED.Api.Repositories.PopularLocationRepositories;
+using RED.Api.Repositories.ProductImageRepositories;
+using RED.Api.Repositories.ProductRepositories;
+using RED.Api.Repositories.PropertyAmenityRepositories;
+using RED.Api.Repositories.ServiceRepositories;
+using RED.Api.Repositories.StatisticsRepositories;
+using RED.Api.Repositories.SubFeatureRepositories;
+using RED.Api.Repositories.TestimonialRepositories;
+using RED.Api.Repositories.ToDoListRepositories;
+using RED.Api.Repositories.WhoWeAreDetailRepositories;
+
+namespace RED.Api.Containers
+{
+    public static class Extensions
+    {
+        public static void ContainerDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<Context>();
+
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+            services.AddTransient<IServiceRepository, ServiceRepository>();
+            services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+            services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+            services.AddTransient<IStatisticsRepository, StatisticsRepository>();
+            services.AddTransient<ITestimonialRepository, TestimonialRepository>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddTransient<IToDoListRepository, ToDoListRepository>();
+            services.AddTransient<IStatisticRepository, StatisticRepository>();
+            services.AddTransient<IChartRepository, ChartRepository>();
+            services.AddTransient<ILast5ProductsRepository, Last5ProductsRepository>();
+            services.AddTransient<IMessageRepository, MessageRepository>();
+            services.AddTransient<IProductImageRepository, ProductImageRepository>();
+            services.AddTransient<IAppUserRepository, AppUserRepository>();
+            services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
+            services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+        }
+    }
+}
--- RED.Api/Program.cs
+++ RED.Api/Program.cs
@@ -1,49 +1,11 @@
+using RED.Api.Containers;
 using RED.Api.Hubs;
-using RED.Api.Models.DapperContext;
-using RED.Api.Repositories.AppUserRepositories;
-using RED.Api.Repositories.BottomGridRepositories;
-using RED.Api.Repositories.CategoryRepositories;
-using RED.Api.Repositories.ContactRepositories;
-using RED.Api.Repositories.EmployeeRepositories;
-using RED.Api.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories;
-using RED.Api.Repositories.EstateAgentRepositories.DashboardRepositories.LastProductsRepositories;
-using RED.Api.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticRepositories;
-using RED.Api.Repositories.MessageRepositories;
-using RED.Api.Repositories.PopularLocationRepositories;
-using RED.Api.Repositories.ProductImageRepositories;
-using RED.Api.Repositories.ProductRepositories;
-using RED.Api.Repositories.PropertyAmenityRepositories;
-using RED.Api.Repositories.ServiceRepositories;
-using RED.Api.Repositories.StatisticsRepositories;
-using RED.Api.Repositories.SubFeatureRepositories;
-using RED.Api.Repositories.TestimonialRepositories;
-using RED.Api.Repositories.ToDoListRepositories;
-using RED.Api.Repositories.WhoWeAreDetailRepositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddTransient<Context>();
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IProductRepository, ProductRepository>();
-builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
-builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
-builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
-builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
-builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
-builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddTransient<IContactRepository, ContactRepository>();
-builder.Services.AddTransient<IToDoListRepository, ToDoListRepository>();
-builder.Services.AddTransient<IStatisticRepository, StatisticRepository>();
-builder.Services.AddTransient<IChartRepository, ChartRepository>();
-builder.Services.AddTransient<ILast5ProductsRepository, Last5ProductsRepository>();
-builder.Services.AddTransient<IMessageRepository, MessageRepository>();
-builder.Services.AddTransient<IProductImageRepository, ProductImageRepository>();
-builder.Services.AddTransient<IAppUserRepository, AppUserRepository>();
-builder.Services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
-builder.Services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+builder.Services.ContainerDependencies();
 
 builder.Services.AddCors(opt =>
 {
